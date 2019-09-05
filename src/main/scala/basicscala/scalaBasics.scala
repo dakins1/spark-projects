fea3302d632fcc13b97a89d0740053aeb4d3f72d
Dillon Akins <dakins1@trinity.edu>
2019-09-04 22:23:08 -0500

    eh

--- src/main/scala/basicscala/scalaBasics.scala
+++ src/main/scala/basicscala/scalaBasics.scala
@@ -23,13 +23,13 @@ object ScalaBasics {
     val p = line.split(",")
     Row(p(0).toInt, p(1), p(2), p(3).toInt, p(4).toInt, p(5).toString, 
             p(6).toInt, p(7), p(8), p(9), p(10).toDouble, p(11).toDouble, p(12).toDouble)
-
      }
   }
 	def main(args: Array[String]): Unit = {
-      val source = scala.io.Source.fromFile("/users/mlewis/workspaceF18/CSCI3395-F18/data/BasicScala/IHME_GLOBAL_EDUCATIONAL_ATTAINMENT_1970_2015_Y2015M04D27.CSV")
+      //val source = scala.io.Source.fromFile("/users/mlewis/workspaceF18/CSCI3395-F18/data/BasicScala/IHME_GLOBAL_EDUCATIONAL_ATTAINMENT_1970_2015_Y2015M04D27.CSV")
+      val source = scala.io.Source.fromFile("/mnt/c/Users/Dillon/comp/datasets/scalaBasics/IHME_GLOBAL_EDUCATIONAL_ATTAINMENT_1970_2015_Y2015M04D27.CSV")
       val lines = source.getLines()
       val data = lines.drop(1).map(parseLine).toArray
-      for (i <- 1 to 10) println(data(i))
+      val p = data.map(_.metric)
     }
 }
