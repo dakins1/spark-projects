94192199de8b1db3472f1febafed71ef443b2a15
Dillon Akins <dakins1@trinity.edu>
2019-09-16 23:25:12 -0500

    Number 10

--- sparkrdd.md
+++ sparkrdd.md
@@ -17,12 +17,15 @@
     Station IN017111200, March 9, 43.61 centimeters
 
 7) Stations associated with San Antonio
-    Assuming "San Antonio" is the rectangular region of 29°36'03.0"N 98°43'14.3"W and 29°13'48.2"N 98°14'08.6"W, there are 115 stations associate with San Antonio. 
+    30
 
 8) San Antonio stations that reported data in 2017
-    58
+    4
 
 9) Largest daily increase for high temps in San Antonio
+    12.2 degrees celsius
 
-
+10) Correlation Coefficient for rain and high temps
+    I calculated the coefficient for each station, then took the average of those coefficients:
+    -0.143
 
--- src/main/scala/sparkrdd/SparkRDD.scala
+++ src/main/scala/sparkrdd/SparkRDD.scala
@@ -21,14 +21,14 @@ object SparkRDD {
     val sc = new SparkContext(conf)
     sc.setLogLevel("WARN")
 
-    val countriesLines = sc.textFile("/users/mlewis/workspaceF18/CSCI3395-F18/data/ghcn-daily/ghcnd-countries.txt")
-    // val countriesLines = sc.textFile("C:\\Users\\Dillon\\comp\\datasets\\sparkRDD\\ghcnd-countries.txt")
+    // val countriesLines = sc.textFile("/users/mlewis/workspaceF18/CSCI3395-F18/data/ghcn-daily/ghcnd-countries.txt")
+    val countriesLines = sc.textFile("C:\\Users\\Dillon\\comp\\datasets\\sparkRDD\\ghcnd-countries.txt")
     val countries = countriesLines.collect.foldLeft((scala.collection.immutable.Map.empty[String, String])){
       (map, line) => map + (line.take(2) -> line.drop(3))
     }
 
-    val stationLines = sc.textFile("/users/mlewis/workspaceF18/CSCI3395-F18/data/ghcn-daily/ghcnd-stations.txt")
-    // val stationLines = sc.textFile("C:\\Users\\Dillon\\comp\\datasets\\sparkRDD\\ghcnd-stations.txt")
+    // val stationLines = sc.textFile("/users/mlewis/workspaceF18/CSCI3395-F18/data/ghcn-daily/ghcnd-stations.txt")
+    val stationLines = sc.textFile("C:\\Users\\Dillon\\comp\\datasets\\sparkRDD\\ghcnd-stations.txt")
     val stationData = stationLines
       .map { line =>
         StationRow(
@@ -41,8 +41,8 @@ object SparkRDD {
         )
       }.cache()
 
-    val reportLines = sc.textFile("/users/mlewis/workspaceF18/CSCI3395-F18/data/ghcn-daily/2017.csv")
-    // val reportLines = sc.textFile("C:\\Users\\Dillon\\comp\\datasets\\sparkRDD\\2017.csv")
+    // val reportLines = sc.textFile("/users/mlewis/workspaceF18/CSCI3395-F18/data/ghcn-daily/2017.csv")
+    val reportLines = sc.textFile("C:\\Users\\Dillon\\comp\\datasets\\sparkRDD\\2017.csv")
     val reportData = reportLines
       .map { line =>
         val p = line.split(",")
@@ -89,7 +89,7 @@ object SparkRDD {
     val indiaMaxRainfall = reportData.filter(r => r.obType == "PRCP" && indiaStationSet.contains(r.id)).sortBy(_.obValue).collect().takeRight(1)(0)
     println(indiaMaxRainfall)
 
-*/
+    */
 
     /* 7. How many stations associated with San Antonio, TX? */
     // val saStations = stationData.filter(s => (s.lat <= 29.6008 && s.lat >= 29.2300) && (s.lon >= -98.7206 && s.lon <= -98.2357))
@@ -98,35 +98,64 @@ object SparkRDD {
 
     /* 8. How many SA stations reported temperature data in 2017? */
     
-    val saReporters = saStations.filter(s => tempReporters.contains(s.id)).count()
-    println("SA reporters: " + saReporters)
+    val saReporters = saStations.filter(s => tempReporters.contains(s.id)).map(_.id).collect().toSet
+    println("SA reporters: " + saReporters.size)
 
     /* 9. Largest increase in daily high temps in SA */
     val saSet = saStations.map(_.id).collect().toSet
     val saReports = reportData.filter(r => saSet.contains(r.id))
     val saTemps = saReports.filter(r => r.obType == "TMAX").collect()
 
-    // val days = saTemps.groupBy(_.mmdd)?
-    val days = saTemps.groupBy(_.id).map(t => (t._1, t._2.groupBy(_.mmdd).map(t => (t._1, t._2.map(_.obValue).max)))
-    // days = (StationID, (Date, MaxTemp for that Station))
-    val maxDays = days.
-    val maxDays = days.map{case (k,v) => (k, v.maxBy(_.obValue))}.toSeq.sortBy(_._2.mmdd).map(_._2)
-
-    val (maxDiff, _) = maxDays.foldLeft((0, maxDays(0))){
-      case ((diff, oldRow), newRow) =>
-        val d = newRow.obValue - oldRow.obValue
-        if (d > diff) (d, newRow)
-        else (diff, newRow)
+    /*
+    val days = saTemps.groupBy(_.id).map(t => (t._1, t._2.groupBy(_.mmdd).map(t => (t._1, t._2.map(_.obValue).max))))
+    val maxDays = days.map(t => (t._1, t._2.toSeq.sortBy(_._1).map(_._2)))
+    
+    val maxDiffs = maxDays.map{
+      case (id, reps) =>
+        val (_, maxDiff) = reps.foldLeft((reps(0), 0)){
+          case ((oldTemp, diff), newTemp) =>
+            val newDiff = newTemp - oldTemp
+            if (newDiff > diff) (newTemp, newDiff)
+            else (newTemp, diff)
+        }
+        (id, maxDiff)
     }
+    // maxDiffs.foreach(println)
+    val maxDiff = maxDiffs.maxBy(_._2)
+    println("Max difference: " + maxDiff)
+    */
+    val days = saTemps.groupBy(_.id).map(t => (t._1, t._2.groupBy(_.mmdd).map(t => (t._1, t._2.map(_.obValue).max))))
+    
+    val saTmaxs = saReports.filter(s => s.obType == "TMAX").groupBy(_.id).map(t => (t._1 -> t._2.toSeq.sortBy(_.mmdd))).collect()
+    val saPrcps = saReports.filter(s=> s.obType == "PRCP" && saReporters.contains(s.id)).groupBy(_.id).map(t => (t._1 -> t._2.toSeq.sortBy(_.mmdd))).collect()
 
-    println("Max diff: " + maxDiff/10.0 + "degress celcius")
+    val pairs = saTmaxs.map{
+      case (id, seq) =>
+       val matchingPrcps = saPrcps.filter(_._1 == id).map(_._2).take(1)(0)
+       val matches = seq.map(r => (r.obValue -> { 
+         val x = matchingPrcps.filter(_.mmdd == r.mmdd)
+         if (x.length == 1) Some(x(0).obValue)
+         else None
+        }))
+       id -> matches.filter(_._2 != None).map(t => (t._1 -> t._2.get))
+    }
+    //first is temp, second is precipitation
+    val coefs = pairs.map{
+      case (id, ps) => 
+        val tAvg = ps.map(_._1).sum / ps.length
+        val pAvg = ps.map(_._2).sum / ps.length
+        val numerator = ps.foldLeft(0){case (s, (t, p)) => s + (t - tAvg)*(p - pAvg)} 
+        val denomLeft = scala.math.sqrt(ps.foldLeft(0.0){case (s, (t, _)) => s + scala.math.pow(t - tAvg, 2)})
+        val denomRight = scala.math.sqrt(ps.foldLeft(0.0){case (s, (_, p)) => s + scala.math.pow(p - pAvg, 2)})
+        (numerator) / (denomLeft * denomRight)
+    }
+    println(coefs.sum / coefs.length)
 
-    /*   
-    Top 
-    29.600833, -98.720639
-    Bottom
-    29.230055, -98.235731
-*/
+    
+
+
+
+
     sc.stop()
   }
 }
