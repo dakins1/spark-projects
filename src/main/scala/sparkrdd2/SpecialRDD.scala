6db9571d28c2e40a8cceeafdead6f4f646cb9963
Dillon Akins <dakins1@trinity.edu>
2019-09-20 15:09:09 -0500

    Number 1

--- src/main/scala/sparkrdd2/SpecialRDD.scala
+++ src/main/scala/sparkrdd2/SpecialRDD.scala
@@ -39,8 +39,24 @@ val conf = new SparkConf().setAppName("Temp Data").setMaster("local[*]")
     // val reports1897 = parseReports(sc.textFile("/users/mlewis/workspaceF18/CSCI3395-F18/data/ghcn-daily/1897.csv"))
 
     /* 1. Station with largest TMAX-TMIN in one day in 2017 */
-    val reportMap = reports2017.map(r => r.id -> (r.mmdd -> r))
-    val maxReport = reportMap.foldByKey()
+    // val reportMap = reports2017.map(r => r.id -> (r.mmdd -> r))
+    val reportPairs = reports2017.map(r => (r.id -> r.mmdd) -> r)
+    val tmaxs = reportPairs.filter(p => p._2.obType == "TMAX")
+    val tmins = reportPairs.filter(p => p._2.obType == "TMIN")
+    val tPairs = tmaxs.join(tmins)
+    val tDiffs = tPairs.map(p => p._1 -> (p._2._2.obValue - p._2._1.obValue))
+    //figure out which observation is first
+    
+    val maxReport2 = tDiffs.fold(tDiffs.first()){
+      case ((key, diff), newPair) =>
+        if (diff > newPair._2) (key, diff)
+        else newPair
+    }
+    println(maxReport2)
+
+
+    // val maxReport = tDiffs
+
     /*
     val maxReport = reports2017.fold(reports2017.first){
       (r1, r2) =>
