0643cbf551e26fce3d1019cba60dc48c48dbdc13
Dillon Akins <dakins1@trinity.edu>
2019-10-17 21:11:38 -0500

    iwhef

--- sparkSQL.md
+++ sparkSQL.md
@@ -1,5 +1,13 @@
 ### 1) How many series does New Mexico have?
 
-264 series
+    * 264 series
 
-### 2) 
+### 2) Max Unemployment Level
+
+    * 28461 units of unemployment
+
+### 3) How many cities with >25k population in NM?
+
+    * 10 cities
+
+### 4) Avg. Unemployment rate in NM 2017 
--- src/main/scala/sparksql/SparkSQL.scala
+++ src/main/scala/sparksql/SparkSQL.scala
@@ -10,13 +10,17 @@ import org.apache.spark.sql.types.IntegerType
 import org.apache.spark.sql.types.StringType
 import org.apache.spark.sql.types.StructField
 import org.apache.spark.sql.types.StructType
+import org.apache.spark.SparkConf
 import swiftvis2.plotting.Plot
 import swiftvis2.plotting._
 import swiftvis2.plotting.renderer.SwingRenderer
+import org.apache.parquet.format.IntType
 
 
 object SparkSQL extends App {
-    val spark = SparkSession.builder().master("local[*]").appName("Temp Data").getOrCreate()
+    val sparkConf = new SparkConf().setAppName("Test")
+    sparkConf.set("spark.sql.crossJoin.enabled", "true")
+    val spark = SparkSession.builder().config(sparkConf).master("local[*]").appName("Temp Data").getOrCreate()
     import spark.implicits._
   
     spark.sparkContext.setLogLevel("WARN")
@@ -26,23 +30,54 @@ object SparkSQL extends App {
         StructField("year", IntegerType),
         StructField("period", StringType),
         StructField("value", DoubleType),
-        StructField("footnode_codes", StringType)
+        StructField("footnote_codes", StringType)
         )
     )
 
     val dataNM = spark.read.schema(dataSchema).
         option("header", "true").
         option("delimiter", "\t").
-    csv("C:/Users/Dillon/comp/datasets/sparksql/la/la.data.38.NewMexico")
-    
+    //csv("C:/Users/Dillon/comp/datasets/sparksql/la/la.data.38.NewMexico")
+    csv("/data/BigData/bls/la/la.data.38.NewMexico")
     /* 1. How many series does NM have? */
+  
+    //csv("C:/Users/Dillon/comp/datasets/sparksql/la/la.data.38.NewMexico")
+
     println("NM has " + dataNM.select('series_id).distinct().count() +" series")
 
-    /* 2. Highest unemployment level of any county or equivalent in NM? */
-    //join with the area data
-    //la.series contains stuff, with series ID
+    /* 2. Highest unemployment level reported in any county in New Mexico for the time series */
+    
+    val areaSchema = StructType(
+        Array(
+            StructField("area_type_code", StringType),
+            StructField("area_code", StringType),
+            StructField("area_text", StringType),
+            StructField("display_level", StringType),
+            StructField("selectable", StringType),
+            StructField("sort_sequence", IntegerType)
+        )
+    )
 
-    //series has the data for a "series", basically a series of observations and their dates
-        //the series ID uniquely identifies a single time series
-    //figure out how to group all this data together
+    val dataArea = spark.read.schema(areaSchema).
+        option("header", "true").
+        option("delimiter", "\t").
+    csv("/data/BigData/bls/la/la.area")
+    
+    val countiesNM = dataArea.filter('area_type_code === "F" && 'area_code.substr(3,2) === "35")
+    val unemployments = dataNM.filter('series_id.substr(19, 2) === "04").select('series_id.substr(4, 15).as("area_code"), 'value) //lol 
+    val joined = countiesNM.join(unemployments, "area_code") //.show()
+    // joined.describe().show()
+    
+    /* 3. How many cities/towns with more than 25,000 people does the BLS track in New Mexico? */
+    val nmCities = dataArea.filter('area_type_code === "G" && 'area_code.substr(3,2) === "35")
+    println("NM cities with >25k population " + nmCities.count())
+   
+    /* 4. What was the average unemployment rate for New Mexico in 2017? Calculate this in three ways: */
+    /* a. Averages of the months for the BLS series for the whole state. */
+    val data2017 = dataNM.filter('year === 2017 && 'series_id.substr(19, 2) === "03").groupBy('period).avg()
+    data2017.show()
+
+    /* b. Simple average of all unemployment rates for counties in the state. */
+
+
 }
