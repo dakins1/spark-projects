7d0b5d78cdaa2cc221a7aaa802ccec935eafbf90
Dillon Akins <dakins1@trinity.edu>
2019-10-25 16:44:09 -0500

    sup

--- sparksql2.md
+++ sparksql2.md
@@ -6,4 +6,13 @@
 
 2. Fraction of counties what went gop by margin of 10% or more? Dems?
 
-    * Gop:  
+    * Gop: 78% of counties won by a margin of 10%.
+    * Dem: 21% of counties won by a margin of 10%.
+
+3. Plots
+
+![hiwe](src/main/scala/sparksql2/vote.png)
+
+That's not too helpful, so here's another with total votes < 70000
+
+![awufhe](src/main/scala/sparksql2/less70000.png)
--- src/main/scala/sparksql2/SparkSQL2.scala
+++ src/main/scala/sparksql2/SparkSQL2.scala
@@ -3,6 +3,9 @@ package sparksql2
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.Encoders
 import swiftvis2.DataSet
+import swiftvis2.plotting._
+import swiftvis2.plotting.renderer.SwingRenderer
+import swiftvis2.plotting.styles.ScatterStyle
 
 case class SeriesEntry(series_id:String, year:Int, period:String,
     value:Double, footnote_codes:String)
@@ -40,6 +43,21 @@ object SparkSQL2 {
       println(voteData.filter(d => d.per_gop > 0.5).count() / voteData.count().toDouble)
   
       /* 2. Fraction of 10% margin */  
-    
+      println(voteData.filter(d => d.per_gop-d.per_dem >= 0.1).count() / voteData.count().toDouble)
+      println(voteData.filter(d => d.per_gop-d.per_dem <= 0.1).count() / voteData.count().toDouble)
+      
+      /* 3. Plot >:( */
+      val x1 = voteData.map(_.total_votes).collect()
+      val y1 = voteData.map(d => d.per_gop - d.per_dem).collect()
+      val p1 = Plot.simple(ScatterStyle(x1, y1, symbolWidth = 8, symbolHeight = 8), "Vote Info of Counties",
+      "# Votes", "% Dem - % GOP")
+
+      val x2 = voteData.filter(_.total_votes < 70000).map(_.total_votes).collect()
+      val y2 = voteData.filter(_.total_votes < 70000).map(d => d.per_gop - d.per_dem).collect()
+      val p2 = Plot.simple(ScatterStyle(x2, y2, symbolWidth = 8, symbolHeight = 8), "Vote Info of Counties",
+      "# Votes", "% Dem - % GOP")
+
+      SwingRenderer(p2, 800, 800, true)
+      val apowefj = 3
     }
 }
