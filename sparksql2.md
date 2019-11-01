307e1c25d04ca1ff5c098a5bbb6ffdab51830b44
Dillon Akins <dakins1@trinity.edu>
2019-11-01 17:41:50 -0500

    bruh

--- sparksql2.md
+++ sparksql2.md
@@ -17,11 +17,32 @@ That's not too helpful, so here's another with total votes < 70000
 
 ![awufhe](src/main/scala/sparksql2/less70000.png)
 
-4. Need to fix and upload this 
+4. 
+
+![aoiwfji](src/main/scala/num4.png)
 
 5. Unemployment Histograms
 
-    * From left to right, the boxes are in chronological order. BDF The first row is metropolitan areas, the second is micropolitan areas, and the third is counties and equivalents.
+    * From left to right, the boxes are in chronological order. The first row is metropolitan areas, the second is micropolitan areas, and the third is counties and equivalents.
     * P.S., you should look at how Tony coded this graph. 
 
 ![ajfwie](src/main/scala/sparksql2/histoGrid.PNG)
+
+6. Correlation between votes and unemployment
+
+    * Correlation of 0.06. To calculate this I used the M13 value, the yearly average of unemployment. Although it does include the month of December, which happened after the election, I thought it would provide a more accurate picture of the correlation. A result of 0.051 implies that the unemployment rate 
+    didn't really influence the election. This makes sense since the economy was pretty stable by Nov. 2016; voters cared more about other matters.  
+
+![aiwefh](src/main/scala/sparksql2/plot2.png)
+
+This shows that less populated areas tend to vote Republican. It also shows that the unemployment rate was not a significant factor in this 
+
+election. 
+
+7. Relationships
+
+    * Based on the results from all the previous questions, it seems like the unemployment rates did not influence the election. The number of votes, however, was correlated with the percentage of democratic votes. In general, larger populations voted more democratic. This makes sense, as metropolitan citizens tend to be more democratic and rural citizens do. The plot from question two shows how smaller populations lean Republican. Here is another plot to back me up:
+
+![faiwejf](src/main/scala/sparksql2/rateVote.png)
+
+As you can see, there is not much of a trend between unemployment rate and political lean.
--- src/main/scala/sparksql2/SparkSQL2.scala
+++ src/main/scala/sparksql2/SparkSQL2.scala
@@ -32,8 +32,9 @@ case class VotesWithGeo(votes_dem:Double, votes_gop:Double, total_votes:Double,
     per_dem:Double, per_gop:Double, state_abbr:String, county_name:String,
 latitude:Double, longitude:Double)
 
-case class JoinedStep(county:String, per_dem:Double)
-case class RatesCountyVotes(rate:Double, county:String, per_dem:Double)
+case class JoinedStep(county:String, area_code:String, per_dem:Double, total_votes:Double)
+case class RatesCountyVotes(rate:Double, county:String, per_dem:Double, total_votes:Double)
+case class VoteAndRate(per_dem:Double, rate:Double, total_votes:Double)
 
 object SparkSQL2 {
     def main(args:Array[String]) = {
@@ -65,19 +66,20 @@ object SparkSQL2 {
       // println(voteData.filter(d => d.per_gop-d.per_dem <= 0.1).count() / voteData.count().toDouble)
       
       /* 3. Plot >:( */
-      /*
-      val x1 = voteData.map(_.total_votes).collect()
-      val y1 = voteData.map(d => d.per_gop - d.per_dem).collect()
-      val p1 = Plot.simple(ScatterStyle(x1, y1, symbolWidth = 8, symbolHeight = 8), "Vote Info of Counties",
-      "# Votes", "% Dem - % GOP")
+      
+      // val x1 = voteData.map(_.total_votes).collect()
+      // val y1 = voteData.map(d => d.per_dem - d.per_gop).collect()
+      // val p1 = Plot.simple(ScatterStyle(x1, y1, symbolWidth = 5, symbolHeight = 5), "Vote Info of Counties",
+      // "# Votes", "% Dem - % GOP")
+      // SwingRenderer(p1, 800, 800, true)
 
-      val x2 = voteData.filter(_.total_votes < 70000).map(_.total_votes).collect()
-      val y2 = voteData.filter(_.total_votes < 70000).map(d => d.per_gop - d.per_dem).collect()
-      val p2 = Plot.simple(ScatterStyle(x2, y2, symbolWidth = 8, symbolHeight = 8), "Vote Info of Counties",
-      "# Votes", "% Dem - % GOP")
+      // val x2 = voteData.filter(_.total_votes < 70000).map(_.total_votes).collect()
+      // val y2 = voteData.filter(_.total_votes < 70000).map(d => d.per_dem - d.per_gop).collect()
+      // val p2 = Plot.simple(ScatterStyle(x2, y2, symbolWidth = 5, symbolHeight = 5), "Vote Info of Counties",
+      // "# Votes", "% Dem - % GOP")
 
-      SwingRenderer(p2, 800, 800, true)
-      */
+      // SwingRenderer(p2, 800, 800, true)
+      
 
       /* 4. Plotting vote results geographically */
       val dataGeo = spark.read.schema(Encoders.product[ZipCodeData].schema).
@@ -87,26 +89,36 @@ object SparkSQL2 {
       //csv("/data/BigData/bls/zip_codes_states.csv").as[ZipCodeData]
       .filter("latitude is not null and longitude is not null")
       
-      /****THIS MIGHT BE WRONG*****/
+      val joined1 = voteData.joinWith(dataGeo, 
+          voteData("state_abbr") === dataGeo("state"))
 
-    //   val joined1 = voteData.joinWith(dataGeo, 
-    //       voteData("state_abbr") === dataGeo("state"))
-          
-    //   val votesWithGeo = joined1.map { case (vd, geo) =>
-    //     VotesWithGeo(vd.votes_dem, vd.votes_gop, vd.total_votes, vd.per_dem, vd.per_gop,
-    //       vd.state_abbr, vd.county_name, geo.latitude, geo.longitude)
-    //   }//.filter(d => d.latitude != null || d.longitude != null)
+      val joined2 = voteData.join(dataGeo, $"state_abbr" === $"state" && $"county_name".contains($"county"))
+      joined2.show()  
 
-    // val cg1 = ColorGradient(0.0 -> RedARGB, 39.9->RedARGB, 
-    //   40.0->MagentaARGB, 59.9->MagentaARGB, 60.0->BlueARGB)      
-    // val p1 = Plot.simple(ScatterStyle(
-    //   votesWithGeo.map(_.longitude).collect(),
-    //   votesWithGeo.map(_.latitude).collect(),
-    //   symbolWidth = 3, symbolHeight = 3,
-    //   colors = votesWithGeo.map(_.per_dem).collect().map(p => cg1(p*100))),
-    //   "Magenta is 40 <= percent democratic <= 60", "Longitude", "Latitude")
+      val x5 = joined2.select($"longitude".as[Double]).collect()
+      val y5 = joined2.select($"latitude".as[Double]).collect()
+      val sz5 = joined2.select($"per_dem".as[Double]).collect()
+      val votesWithGeo = joined1.map { case (vd, geo) =>
+        VotesWithGeo(vd.votes_dem, vd.votes_gop, vd.total_votes, vd.per_dem, vd.per_gop,
+          vd.state_abbr, vd.county_name, geo.latitude, geo.longitude)
+      }//.filter(d => d.latitude != null || d.longitude != null)
 
-    // SwingRenderer(p1, 800, 800, true)
+    val cg1 = ColorGradient(0.0 -> RedARGB, 39.9->RedARGB, 
+      40.0->MagentaARGB, 59.9->MagentaARGB, 60.0->BlueARGB)      
+    val p5 = Plot.simple(ScatterStyle(
+      votesWithGeo.map(_.longitude).collect(),
+      votesWithGeo.map(_.latitude).collect(),
+      symbolWidth = 3, symbolHeight = 3,
+      colors = votesWithGeo.map(_.per_dem).collect().map(p => cg1(p*100))),
+      "Magenta is 40 <= percent democratic <= 60", "Longitude", "Latitude")
+      val p7 = Plot.simple(ScatterStyle(
+        x5,
+        y5,
+        symbolWidth = 3, symbolHeight = 3,
+        colors = sz5.map(p => cg1(p*100))),
+        "Magenta is 40 <= percent democratic <= 60", "Longitude", "Latitude")
+
+    SwingRenderer(p7, 1128, 800, true)
     
     /* 5. Historgrams of Recessions */
     //Make a function and paramaterize by month, year, before/after recession, 
@@ -124,17 +136,19 @@ object SparkSQL2 {
     val dataArea = spark.read.schema(Encoders.product[AreaData].schema).
       option("header", "true").
       option("delimiter", "\t").
-      csv("C:\\Users\\Dillon\\comp\\datasets\\sparksql\\la\\la.area").as[AreaData]
+      csv("C:\\Users\\Dillon\\comp\\datasets\\sparksql\\la\\la.area").as[AreaData].map{d =>
+        AreaData(d.area_type_code, d.area_code, d.area_text.dropRight(4), d.display_level, d.selectable, d.sort_sequence)
+      }
 
-    // val bins = (0.1 to 50.0 by 1.0).toArray
+    // val bins = (0.0 to 50.0 by 1.0).toArray
     // def getHisto(month:String, year:Int, bOrA:Boolean, area_type:String):DataAndColor = {
     //   val series = dataSeries.filter(
     //     d => d.period==month && d.year==year && d.series_id.takeRight(2) == "03") 
     //   val areas = dataArea.filter(r => r.area_type_code==area_type)
-    //   val joined = series.joinWith(areas, series("area_code1") === areas("area_code"))
-    //   val counts = joined.map(_._1.value).rdd.histogram(bins, true)
-    //   println(month, year, area_type)
-    //   DataAndColor(counts, if (bOrA) GreenARGB else RedARGB)
+      // val joined = series.joinWith(areas, series("area_code1") === areas("area_code"))
+      // val counts = joined.map(_._1.value).rdd.histogram(bins, true)
+      // println(month, year, area_type)
+      // DataAndColor(counts, if (bOrA) GreenARGB else RedARGB)
     // }
 
     // val dateParams = Array(
@@ -146,7 +160,7 @@ object SparkSQL2 {
     // val histos = (for (a <- areaParams) yield {
     //   (for (d <- dateParams) yield getHisto(d._1, d._2, d._3, a)).toSeq
     // }).toSeq
-    // // val histos = getHisto("M12", 2007, true, "B")
+    // val histos = getHisto("M12", 2007, true, "B")
     // val grid = Plot.histogramGrid(bins, histos, true, false, "Unemployment Rates", "Date", "Rates")
     // SwingRenderer(grid, 1000, 1000, true)
 
@@ -154,18 +168,48 @@ object SparkSQL2 {
       //Correlation coefficient between unemployment and democratic votes for all counties
       //gotta join a county with it's voting percentage
       val counties = dataArea.filter(_.area_type_code == "F")
-      val unempRates = dataSeries.filter(_.series_id.takeRight(2) == "03")
+      val unempRates = dataSeries.filter(d => d.series_id.takeRight(2) == "03" && d.year==2016 && d.period=="M11")
+      println("rates")
+      unempRates.show(false)
       //This is only joining on county names, could possibly include state names
 
       //join votes and counties, then join the area codes with the series measurements. You'll also have to filter out
-      //non-2016 years. Filter out to calculate with the M13 values. Acknowledge that this throws it off by one month, 
-      //but shouldn't be a big deal. It's not like a recession happened right after. 
-      val countiesAndVotes = counties.joinWith(voteData, counties("area_text") === voteData("county_name")).
-        map(d => JoinedStep(d._2.county_name, d.))
+      //non-2016 years. 
+      val countiesAndVotes = counties.joinWith(voteData, counties("area_text") === voteData("county_name")).map{
+        d => JoinedStep(d._2.county_name, d._1.area_code, d._2.per_dem, d._2.total_votes)
+      }
+      countiesAndVotes.filter(_.area_code.contains("C")).show(false)
+      countiesAndVotes.show(false)
 
-      val cvs = countiesAndVotes.joinWith(unempRates, countiesAndVotes)
+      val votesAndRates = countiesAndVotes.joinWith(unempRates, countiesAndVotes("area_code") === unempRates("area_code1")).map{
+        d => VoteAndRate(d._1.per_dem, d._2.value, d._1.total_votes)
+      }
+      votesAndRates.show(false)
+
+      
+      // val vMean = votesAndRates.map(_.per_dem).rdd.mean()
+      // val rMean = votesAndRates.map(_.rate).rdd.mean()
+      // val numer = votesAndRates.map(d => (d.per_dem - vMean) * (d.rate - rMean)).rdd.sum() 
+      // val denom1 = math.sqrt(votesAndRates.map(d => (d.per_dem - vMean) * (d.per_dem - vMean)).rdd.sum())
+      // val denom2 = math.sqrt(votesAndRates.map(d => (d.rate - rMean) * (d.rate - rMean)).rdd.sum())
+      // println(numer / (denom1*denom2))
 
 
+    /* 6a. Plotting - unemployment x party vote, size = population */
+    // val cg2 = ColorGradient(.4 -> RedARGB, .6 -> BlueARGB)
+    // val cls = votesAndRates.collect().map(d => cg2(d.per_dem))
+    // val p3 = Plot.simple(
+    //   ScatterStyle(votesAndRates.map(_.total_votes).collect(), votesAndRates.map(_.rate).collect(),
+    //     symbolWidth = 3, symbolHeight = 3, colors = cls), "60% Dem is Blue, 40% is Red", "Total Votes", "Unemployment Rate")
+    // SwingRenderer(p3, 800, 800, true)
+
+    /* 7. Relationships */
+
+    // val p4 = Plot.simple(
+    //   ScatterStyle(votesAndRates.map(_.rate).collect(), votesAndRates.map(_.per_dem*100).collect()),
+    //   "Relationship", "Unemployment", "% Democratic"
+    // )
+    // SwingRenderer(p4, 800, 800, true)
     val goodLord = ""
   }
 }
