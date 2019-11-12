8668a7d20b20210d17acdc1cc2091f6d0308b855
Dillon Akins <dakins1@trinity.edu>
2019-11-12 14:29:23 -0600

    #3

--- sparkml-regressions.md
+++ sparkml-regressions.md
@@ -1,2 +1,8 @@
 1. How many columns in bottle.csv have more than half the rows non-null? 
     * 36 columns
+
+2. Average bottle count per cast?
+    * 25 bottles per cast
+
+3. Plot of casts
+![aiwejof](src/main/scala/sparkml/casts.png)
--- src/main/scala/sparkml/MLRegression.scala
+++ src/main/scala/sparkml/MLRegression.scala
@@ -37,10 +37,32 @@ object MLRegression {
         csv("C:/Users/Dillon/comp/datasets/oceans/cast.csv")
 
         /* 1. How many columns in bottle.csv have data for at least half the rows? */ 
-        // bottles.summary("count").show()
-        val totalRows = bottles.count()
-        val counts = bottles.summary("count").first()
-        val noNulls = (for (i <- 1 to counts.length-1) yield if (counts.get(i).toString.toInt > totalRows / 2) 1 else 0).sum
-        println(noNulls)
+        // val totalRows = bottles.count()
+        // val counts = bottles.summary("count").first()
+        // val noNulls = (for (i <- 1 to counts.length-1) yield if (counts.get(i).toString.toInt > totalRows / 2) 1 else 0).sum
+        // println(noNulls)
+
+        // /* 2. Avg. bottle count per cast */ 
+        // val cnts = bottles.groupBy("Cst_Cnt").agg(count($"Btl_Cnt").as("cnts")).agg(avg($"cnts")).show()
+        // println("Avg. bottles per count " + cnts)
+    
+        /* 3. Locations of casts */
+        // val newCast = cast.filter('Lat_Dec =!= null && 'Lon_Dec =!= null)
+        val preNewCast = cast.filter('Lat_Dec.isNotNull && 'Lon_Dec.isNotNull) 
+        val newCast = cast.join(bottles, "Cst_Cnt").filter('Depthm.isNotNull && 'T_degC.isNotNull)
+        val longs = newCast.select('Lon_Dec.as[Double]).collect()
+        val lats = newCast.select('Lat_Dec.as[Double]).collect()
+        val maxMin = newCast.select('Depthm.as[Int].as("d")).agg(max($"d"), min($"d")).first()
+        val sz = ColorGradient(maxMin.getInt(0).toDouble -> 1, maxMin.getInt(1).toDouble -> 10)
+        val sizes = newCast.select('Depthm.as[Int]).collect().map(d => (d/300.0).toInt + 2)
+        // val sizes = newCast.select('Depthm.as[Int]).collect().map(d => sz(d))
+        val cg = ColorGradient(0.0 -> BlueARGB, 12.0 -> RedARGB)
+        val colors = newCast.select('T_degC.as[Double]).collect().map(t => cg(t))
+        val p = Plot.simple(
+            ScatterStyle(longs, lats, symbolWidth = sizes, symbolHeight = sizes, colors = colors), 
+            "Casts", "Long.", "Lat."
+        )
+        SwingRenderer(p, 800, 800, true)
+
     }
 }
