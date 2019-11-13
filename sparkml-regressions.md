eeb49f7b51cad79e57cf1452b6ff9108af67bd7e
Dillon Akins <dakins1@trinity.edu>
2019-11-12 19:20:08 -0600

    #5

--- sparkml-regressions.md
+++ sparkml-regressions.md
@@ -9,3 +9,11 @@
 Blue represents 0C and red represents 12C
 
 ![aiwejof](src/main/scala/sparkml/casts.png)
+
+4. Linear Regression of temperature to salinity
+
+    * Average error of 0.279 g/kg
+
+5. Linear regression of temperature, depth, and O2ml_L to salinity
+
+    * Average error of 2.5E-13, so really 0, g/kg. This means our training model is spot on with its predictions. 
--- src/main/scala/sparkml/MLRegression.scala
+++ src/main/scala/sparkml/MLRegression.scala
@@ -19,6 +19,8 @@ import swiftvis2.plotting.styles.ScatterStyle
 import swiftvis2.plotting._
 import swiftvis2.plotting.renderer.SwingRenderer
 import org.apache.parquet.format.IntType
+import org.apache.spark.ml.feature.VectorAssembler
+import org.apache.spark.ml.regression.LinearRegression
 
 object MLRegression {
     def main(args:Array[String]) {
@@ -50,19 +52,51 @@ object MLRegression {
         // val newCast = cast.filter('Lat_Dec =!= null && 'Lon_Dec =!= null)
         val preNewCast = cast.filter('Lat_Dec.isNotNull && 'Lon_Dec.isNotNull) 
         val newCast = cast.join(bottles, "Cst_Cnt").filter('Depthm.isNotNull && 'T_degC.isNotNull)
-        val longs = newCast.select('Lon_Dec.as[Double]).collect()
-        val lats = newCast.select('Lat_Dec.as[Double]).collect()
-        val maxMin = newCast.select('Depthm.as[Int].as("d")).agg(max($"d"), min($"d")).first()
-        val sz = ColorGradient(maxMin.getInt(0).toDouble -> 1, maxMin.getInt(1).toDouble -> 10)
-        val sizes = newCast.select('Depthm.as[Int]).collect().map(d => (d/300.0).toInt + 2)
-        // val sizes = newCast.select('Depthm.as[Int]).collect().map(d => sz(d))
-        val cg = ColorGradient(0.0 -> BlueARGB, 12.0 -> RedARGB)
-        val colors = newCast.select('T_degC.as[Double]).collect().map(t => cg(t))
-        val p = Plot.simple(
-            ScatterStyle(longs, lats, symbolWidth = sizes, symbolHeight = sizes, colors = colors), 
-            "Casts", "Long.", "Lat."
-        )
-        SwingRenderer(p, 800, 800, true)
+        // val longs = newCast.select('Lon_Dec.as[Double]).collect()
+        // val lats = newCast.select('Lat_Dec.as[Double]).collect()
+        // val maxMin = newCast.select('Depthm.as[Int].as("d")).agg(max($"d"), min($"d")).first()
+        // val sz = ColorGradient(maxMin.getInt(0).toDouble -> 1, maxMin.getInt(1).toDouble -> 10)
+        // val sizes = newCast.select('Depthm.as[Int]).collect().map(d => (d/300.0).toInt + 2)
+        // // val sizes = newCast.select('Depthm.as[Int]).collect().map(d => sz(d))
+        // val cg = ColorGradient(0.0 -> BlueARGB, 12.0 -> RedARGB)
+        // val colors = newCast.select('T_degC.as[Double]).collect().map(t => cg(t))
+        // val p = Plot.simple(
+        //     ScatterStyle(longs, lats, symbolWidth = sizes, symbolHeight = sizes, colors = colors), 
+        //     "Casts", "Long.", "Lat."
+        // )
+        // SwingRenderer(p, 800, 800, true)
 
+        /* 4. Linear regression: temperature -> salinity */
+        // val newNewCast = newCast.filter('Salnty.isNotNull)
+        // val holUp = newNewCast.select('T_degC.as("temperature"), 'Salnty.as("salinity"))
+        // val va = new VectorAssembler()
+            // .setInputCols(Array("temperature"))
+            // .setOutputCol("featLilWayne")
+        // val temps = va.transform(holUp)
+        // temps.show(false)
+
+        // val lr = new LinearRegression()
+            // .setFeaturesCol("featLilWayne")
+            // .setLabelCol("salinity")
+        // val lrModel = lr.fit(temps)
+        // println(lrModel.coefficients)
+        // val fitData = lrModel.transform(temps)
+        // val god = fitData.select(expr("salinity - prediction").as("diff")).agg(sum("diff")).first()
+        // println("Avg. err: " + god.getDouble(0) / fitData.count())
+        // println("Mean abs err: "+lrModel.summary.meanAbsoluteError)
+    
+        /* 5. LR w/ Depth and O2ml_L */
+        val newData = newCast.select('T_degC, 'Salnty.as("salinity"), 'Depthm, 'O2ml_L).filter(row => !row.anyNull)
+        val va1 = new VectorAssembler()
+            .setInputCols(Array("Depthm", "T_degC", "salinity", "O2ml_L"))
+            .setOutputCol("featLilJon")
+        val avengersAssembled = va1.transform(newData)
+        val lr1 = new LinearRegression()
+            .setFeaturesCol("featLilJon")
+            .setLabelCol("salinity")
+        val lrModel1 = lr1.fit(avengersAssembled)
+        val fitData1 = lrModel1.transform(avengersAssembled)
+        fitData1.show()
+        println("New avg. err: " + lrModel1.summary.meanAbsoluteError)
     }
 }
